e5b6786b7da5cd7f86ce9d6b50423bb98264b13e
Caio Leão <caio.marinho@edu.pe.senac.br>
2024-10-14 16:04:33 -0300

    Ignorar

--- Scripts/States/TristeState.cs
+++ Scripts/States/TristeState.cs
@@ -3,18 +3,25 @@ using System;
 
 public partial class TristeState : BaseState
 {
-    public override void EnterState(StateManager State)
-    {
-        State.animation.Play("TristeState");
-    }
+	public override void EnterState(StateManager State)
+	{
+		State.animation.Play("TristeState");
+	}
 
-    public override void UpdateState(StateManager State)
-    {
-        return;
-    }
+	public override void UpdateState(StateManager State)
+	{
+		if (Input.IsActionJustPressed("PreviousInput"))
+		{
+			State.SwitchState(State.inicialState);
+		}
+		if (Input.IsActionJustPressed("NextInput"))
+		{
+			State.SwitchState(State.inicialState);
+		}
+	}
 
-    public override void LeaveState(StateManager State)
-    {
-        
-    }
+	public override void LeaveState(StateManager State)
+	{
+		
+	}
 }
