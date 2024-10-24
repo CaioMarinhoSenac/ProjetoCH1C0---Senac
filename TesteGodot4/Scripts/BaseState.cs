3fbb41f400e12161f918b9119875e6967b0f843e
CaioMarinhoSenac <caio.marinho@edu.pe.senac.br>
2024-10-24 13:41:34 +0100

    Limpando pasta do projeto

--- TesteGodot4/Scripts/BaseState.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-
-public abstract class BaseState
-{
-	public abstract void EnterState(StateManager State);
-	public abstract void UpdateState(StateManager State);
-	public abstract void LeaveState(StateManager State);
-}
