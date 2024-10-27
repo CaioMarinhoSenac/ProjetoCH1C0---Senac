64fc7323f8e78ff0a17bf4475b6ebfecf9e53acf
CaioMarinhoSenac <caio.marinho@edu.pe.senac.br>
2024-10-27 11:16:43 -0300

    Implementando Cor para Ciclo do Dia

--- Scripts/CanvasScript.cs
+++ Scripts/CanvasScript.cs
@@ -3,10 +3,26 @@ using System;
 
 public partial class CanvasScript : CanvasLayer
 {
+	[Export] public CanvasModulate canvasModulate;
+	
+	[Export] public Gradient gradient;
+
 	public override void _Process(double delta)
 	{
 		if(Input.IsActionJustPressed("ui_cancel")){
 			GetTree().Quit();
 		}
+
+		SearchDayTime();
+	}
+
+	private void SearchDayTime(){
+		var systemTime = Time.GetTimeDictFromSystem();
+
+		float timeValue = Convert.ToSingle($"{systemTime["hour"]:00}{systemTime["minute"]:00}");
+
+		float timePercent = timeValue / 2400.0f;
+
+		canvasModulate.Color = gradient.Sample(timePercent);
 	}
 }
