275a4eeb20c4501f54fedb8ab0e49b4bdb495503
CaioMarinhoSenac <caio.marinho@edu.pe.senac.br>
2024-10-29 14:02:57 -0300

    Desativar Mouse ao iniciar

--- Scripts/CanvasScript.cs
+++ Scripts/CanvasScript.cs
@@ -4,19 +4,25 @@ using System;
 public partial class CanvasScript : CanvasLayer
 {
 	[Export] public CanvasModulate canvasModulate;
-	
+
 	[Export] public Gradient gradient;
 
-	public override void _Process(double delta)
+
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Hidden;
+    }
+
+    public override void _Process(double delta)
 	{
 		if(Input.IsActionJustPressed("ui_cancel")){
 			GetTree().Quit();
 		}
 
-		SearchDayTime();
+		SearchColorDayTime();
 	}
 
-	private void SearchDayTime(){
+	private void SearchColorDayTime(){
 		var systemTime = Time.GetTimeDictFromSystem();
 
 		float timeValue = Convert.ToSingle($"{systemTime["hour"]:00}{systemTime["minute"]:00}");
