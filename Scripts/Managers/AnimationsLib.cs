aede979ec00e5bcac669e42c2164a39b07285635
Caio Leão <caio.marinho@edu.pe.senac.br>
2024-11-04 17:26:07 -0300

    Breve otimização Tween de SwitchColor
    Talvez haja vazamento de memória, rezemos para que não.

--- Scripts/Managers/AnimationsLib.cs
+++ Scripts/Managers/AnimationsLib.cs
@@ -30,19 +30,21 @@ public partial class AnimationsLib : Node
 
     public void SwitchColor(Sprite2D target, Color targetColor, float speed, bool loop)
     {
+        Tween tween = target.CreateTween();
+
         if(!loop)
         {
-            Tween tween = target.CreateTween().SetLoops(0);
+            tween.SetLoops(0);
 
             tween.TweenProperty(target, "modulate", targetColor, speed);
         }
         else
         {
-            Tween tween = target.CreateTween().SetLoops();
+            tween.SetLoops();
 
-            tween.TweenProperty(target, "modulate", new Color(1, 0.65f, 0, 0.5f), 0.75f);
-            tween.TweenProperty(target, "modulate", new Color(1.0f, 0.41f, 0.71f, 0.5f), 0.75f);
-            tween.TweenProperty(target, "modulate", new Color(0.0f, 1.0f, 0.0f, 0.5f), 0.75f);
+            tween.TweenProperty(target, "modulate", new Color(1, 0.65f, 0), 0.4f);
+            tween.TweenProperty(target, "modulate", new Color(1.0f, 0.41f, 0.71f), 0.4f);
+            tween.TweenProperty(target, "modulate", new Color(0.0f, 1.0f, 0.0f), 0.4f);
         }
     }
 }
